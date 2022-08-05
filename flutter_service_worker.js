2dad0f94fe3d96a1699aa06589adf2c5358c715d
FilloPrinci <filloprinci98@gmail.com>
2022-08-05 18:03:37 +0200

    Feat: working on gallery

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,9 +29,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8bd8780845be18592c8e66c0422f4907",
-"/": "8bd8780845be18592c8e66c0422f4907",
-"main.dart.js": "5e32ca9026034241b309aa0d83d69cd7",
+"index.html": "8e9bf0df9cbc3a9095d2930580852763",
+"/": "8e9bf0df9cbc3a9095d2930580852763",
+"main.dart.js": "ea1cb378555818e3d0ac45ab055582ba",
 "manifest.json": "5981b009923e6f6894ee19a042e450b1",
 "version.json": "929904b07fcc7b8b747486a30dedf473"
 };
